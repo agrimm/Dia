473d4e678a0f48de246876831950368b497e484c
robgleeson <rob@flowof.info>
2010-07-22 02:21:55 +0100

    Add test suite for #redirect_stderr=.
    Asserts its behavior in the documentation is correct.

---
+++ test/suite/lib/dia/RubyBlock#redirect_stderr=.rb
@@ -0,0 +1,17 @@
+suite('RubyBlock') do
+
+  suite('#redirect_stderr=') do
+
+    exercise('#redirect_stderr= returns') do
+      dia = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) { }
+      @a = dia.redirect_stderr = true
+      @b = dia.redirect_stderr = false
+    end
+
+    verify('its calling argument') do
+      @a == true && @b == false
+    end
+
+  end
+
+end
